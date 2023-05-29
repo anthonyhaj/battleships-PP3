66f3678586b131c48f52e4f264f1fa798ac4a83b
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-05-29 21:17:08 +0000

    add function to display game board and print cell contents based on value

--- run.py
+++ run.py
@@ -15,7 +15,27 @@ miss_cell = '-'
 # Ship sizes 
 ship_sizes = {'Carrier': 5, 'Battleship': 4, 'Cruiser': 3, 'Submarine': 3, 'Destroyer': 2}
 
-# Initialize the game board
+# Initialize the game boards for player and computer
 player_board = [[empty_cell] * board_size for _ in range(board_size)]
 computer_board = [[empty_cell] * board_size for _ in range(board_size)]
 
+# Function to display the game board
+def display_board(board, hide_ships=False):
+    """
+        Displays the game board.
+        hide_ships: Whether to hide the ships on the board.
+    """
+    print('   ' + ' '.join(chr(ord('A') + i) for i in range(board_size)))
+    for row in range(board_size):
+        print(f'{row + 1:2d} ', end='')
+        for col in range(board_size):
+            cell = board[row][col]
+            if cell == hit_cell or cell == miss_cell:
+                print(cell + ' ', end='')
+            elif cell == empty_cell:
+                print(empty_cell + ' ', end='')
+            elif hide_ships and cell == ship_cell:
+                print(empty_cell + ' ', end='')
+            else:
+                print(cell + ' ', end='')
+        print()
